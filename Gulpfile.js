d0f9a84f3d0cbe3ed42cadf189a7f5a92fdfeb62
Alejandro Pereyra <alepereyra30@gmail.com>
2015-06-24 22:51:46 -0300

    fileconfig

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,7 +7,11 @@ var gulp = require('gulp'),
     inject = require('gulp-inject'),
     less = require('gulp-less'),
     minifyCss = require('gulp-minify-css'),
-    wiredep = require('wiredep').stream;
+    wiredep = require('wiredep').stream,
+    handlebars = require('gulp-handlebars'),
+    wrap = require('gulp-wrap'),
+    declare = require('gulp-declare'),
+    concat = require('gulp-concat');
 
 
 // Servidor web de desarrollo
@@ -16,10 +20,21 @@ gulp.task('server', function() {
         root: './app',
         hostname: '0.0.0.0',
         port: 8080,
-        livereload: true,
+        livereload: true
     });
 });
 
+gulp.task('templates', function(){
+  gulp.src('app/templates/**/*.hbs')
+    .pipe(handlebars())
+    .pipe(wrap('Handlebars.template(<%= contents %>)'))
+    .pipe(declare({
+      namespace: 'utnExpress.templates',
+      noRedeclare: true, // Avoid duplicate declarations 
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('app/js/backbone/'));
+});
 
 
 // Busca en las carpetas de estilos y javascript los archivos que hayamos creado
@@ -28,15 +43,35 @@ gulp.task('inject', function() {
     var sources = gulp.src([
         './app/js/**/*.js',
         './app/css/**/*.css',
-        '!./app/js/main.js', 
+        '!./app/js/backbone/models/**/*.js',
+        '!./app/js/backbone/collections/**/*.js',
+        '!./app/js/backbone/views/**/*.js',
+        '!./app/js/backbone/main.js', 
         '!./app/js/vendor/**/*.js' 
     ]);
-    var importantFile = gulp.src(['./app/js/main.js']);
+    var modelsFile = gulp.src(['./app/js/backbone/models/**/*.js']);
+    var collectionsFile = gulp.src(['./app/js/backbone/collections/**/*.js']);
+    var viewsFile = gulp.src(['./app/js/backbone/views/**/*.js']);
+    var importantFile = gulp.src(['./app/js/backbone/main.js']);
     
     return gulp.src('index.html', {
         cwd: './app'
     })
-
+    .pipe(inject(modelsFile, {
+        read: false,
+        ignorePath: '/app',
+        name: 'models'
+    }))
+    .pipe(inject(collectionsFile, {
+        read: false,
+        ignorePath: '/app',
+        name: 'collections'
+    }))
+    .pipe(inject(viewsFile, {
+        read: false,
+        ignorePath: '/app',
+        name: 'views'
+    }))
     .pipe(inject(importantFile, {
         read: false,
         ignorePath: '/app',
@@ -68,7 +103,6 @@ gulp.task('copy', function() {
 gulp.task('html', function() {
     gulp.src('./app/**/*.html')
         .pipe(connect.reload());
-
 });
 
 gulp.task('less', function () {
@@ -80,8 +114,9 @@ gulp.task('less', function () {
 gulp.task('watch', function() {
     gulp.watch(['./app/less/**/*.less'], ['less']);
     gulp.watch(['./app/**/*.html'], ['html']);
-    gulp.watch(['./app/js/**/*.js']);
+    gulp.watch(['./app/js/**/*.js'], ['inject']);
     gulp.watch(['./app/css/**/*.css'], ['inject']);
+    gulp.watch(['./app/templates/**/*.hbs'], ['templates']);
     gulp.watch(['./bower.json'], ['wiredep']);
 });
 
